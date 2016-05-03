508e8ae7a5108d42da384fd77da64ab7b7c5930d
Tamás Gulácsi <tgulacsi78@gmail.com>
2016-05-03 22:51:32 +0200

    generated.proto compiles

--- structs/proto.go
+++ structs/proto.go
@@ -17,13 +17,16 @@ limitations under the License.
 package structs
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"strings"
 
 	"gopkg.in/errgo.v1"
-	"gopkg.in/inconshreveable/log15.v2"
 )
+
+//go:generate go get github.com/golang/protobuf/protoc-gen-go
+// https://github.com/google/protobuf/releases/download/v3.0.0-beta-2/protoc-3.0.0-beta-2-linux-x86_64.zip
 
 func SaveProtobuf(dst io.Writer, functions []Function, pkg string) error {
 	var err error
@@ -48,11 +51,12 @@ FunLoop:
 				return err
 			}
 		}
+		name := dot2D.Replace(fun.Name())
 		fmt.Fprintf(w, `
 service %s {
 	rpc %s (%s) returns (%s) {}
 }
-`, fun.Name(), fun.Name(), fun.getStructName(false), fun.getStructName(true))
+`, name, name, strings.ToLower(fun.getStructName(false)), strings.ToLower(fun.getStructName(true)))
 	}
 
 	return nil
@@ -63,11 +67,7 @@ func (f Function) SaveProtobuf(dst io.Writer, out bool) error {
 	if out {
 		dirmap, dirname = DIR_OUT, "output"
 	}
-	var (
-		err        error
-		aName, got string
-	)
-	args := make([]Argument, 0, len(f.Args))
+	args := make([]Argument, 0, len(f.Args)+1)
 	for _, arg := range f.Args {
 		if arg.Direction&dirmap > 0 {
 			args = append(args, arg)
@@ -78,25 +78,87 @@ func (f Function) SaveProtobuf(dst io.Writer, out bool) error {
 		args = append(args, *f.Returns)
 	}
 
+	return protoWriteMessageTyp(dst, dot2D.Replace(strings.ToLower(f.Name()))+"__"+dirname, f.types, args...)
+}
+
+var dot2D = strings.NewReplacer(".", "__")
+
+func protoWriteMessageTyp(dst io.Writer, msgName string, types map[string]string, args ...Argument) error {
+	var err error
 	w := errWriter{Writer: dst, err: &err}
 
-	fmt.Fprintf(w, `
-message %s__%s {
-	`, f.Name(), dirname,
-	)
+	fmt.Fprintf(w, "\nmessage %s {\n", msgName)
 
-	Log.Debug("SaveProtobuf",
-		"function", log15.Lazy{func() string { return fmt.Sprintf("%#v", f) }})
+	seen := make(map[string]struct{}, 16)
+	buf := buffers.Get()
+	defer buffers.Put(buf)
 	for i, arg := range args {
-		if arg.Flavor == FLAVOR_TABLE && arg.TableOf == nil {
-			return errgo.WithCausef(nil, ErrMissingTableOf, "no table of data for %s.%s (%v)", f.Name(), arg, arg)
+		if strings.HasSuffix(arg.Name, "#") {
+			continue
 		}
-		aName = capitalize(goName(arg.Name))
-		got = arg.goType(f.types, arg.Flavor == FLAVOR_TABLE)
-		//lName := strings.ToLower(arg.Name)
-		fmt.Fprintf(w, "\t%s %s = %d;\n", strings.TrimPrefix(got, "*"), aName, i)
+		if arg.Flavor == FLAVOR_TABLE && arg.TableOf == nil {
+			return errgo.WithCausef(nil, ErrMissingTableOf, "no table of data for %s.%s (%v)", msgName, arg, arg)
+		}
+		aName := arg.Name
+		got := arg.goType(types, false)
+		var rule string
+		if strings.HasPrefix(got, "[]") {
+			rule = "repeated "
+			got = got[2:]
+		}
+		if strings.HasPrefix(got, "*") {
+			got = got[1:]
+		}
+		typ := protoType(got)
+		if arg.Flavor == FLAVOR_SIMPLE || arg.Flavor == FLAVOR_TABLE && arg.TableOf.Flavor == FLAVOR_SIMPLE {
+			fmt.Fprintf(w, "\t%s%s %s = %d;\n", rule, typ, aName, i+1)
+			continue
+		}
+		if _, ok := seen[typ]; !ok {
+			//lName := strings.ToLower(arg.Name)
+			subArgs := make([]Argument, 0, 16)
+			if arg.TableOf != nil {
+				if arg.TableOf.RecordOf == nil {
+					subArgs = append(subArgs, *arg.TableOf)
+				} else {
+					for _, v := range arg.TableOf.RecordOf {
+						subArgs = append(subArgs, v)
+					}
+				}
+			} else {
+				for _, v := range arg.RecordOf {
+					subArgs = append(subArgs, v)
+				}
+			}
+			buf.Reset()
+			if err := protoWriteMessageTyp(buf, typ, types, subArgs...); err != nil {
+				return err
+			}
+			w.Write(bytes.Replace(buf.Bytes(), []byte("\n"), []byte("\n\t"), -1))
+			seen[typ] = struct{}{}
+		}
+		fmt.Fprintf(w, "\n\t%s%s %s = %d;\n", rule, typ, aName, i+1)
 	}
-	io.WriteString(w, "}\n")
+	io.WriteString(w, "}")
 
 	return err
+}
+
+func protoType(got string) string {
+	switch strings.ToLower(got) {
+	case "ora.date":
+		return "string"
+	case "ora.string":
+		return "string"
+	case "int32":
+		return "sint32"
+	case "ora.int32":
+		return "sint32"
+	case "float64":
+		return "double"
+	case "ora.float64":
+		return "double"
+	default:
+		return strings.ToLower(strings.TrimPrefix(strings.TrimPrefix(got, "[]"), "*"))
+	}
 }
