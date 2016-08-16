8a051194fcab5ae4d23bafa8e2f48fcb7fd2f640
Tamás Gulácsi <T.Gulacsi@unosoft.hu>
2016-08-16 15:38:51 +0200

    extract include to $GOPATH/, too

--- structs/download-protoc.sh
+++ structs/download-protoc.sh
@@ -2,13 +2,23 @@
 set -u
 set -e
 set -x
-protoc --version 2>/dev/null | fgrep 'libprotoc 3' || {
-curl -L https://github.com/google/protobuf/releases/download/v3.0.0/protoc-3.0.0-linux-x86_64.zip -o /tmp/protoc-3.0.0-linux-x86_64.zip && unzip -p /tmp/protoc-3.0.0-linux-x86_64.zip bin/protoc >$HOME/bin/protoc
-chmod 0755 $HOME/bin/protoc
-}
+tmpdir="${TMPDIR:-/tmp}/$(basename $0)-$$"
+mkdir -p "$tmpdir"
+if ! protoc --version 2>/dev/null | fgrep 'libprotoc 3'; then
+	dir=$(pwd)
+	cd "$tmpdir"
+	curl -L https://github.com/google/protobuf/releases/download/v3.0.0/protoc-3.0.0-linux-x86_64.zip \
+		-o protoc-3.0.0-linux-x86_64.zip
+	unzip /tmp/protoc-3.0.0-linux-x86_64.zip
+	chmod 0755 bin/protoc
+	mv bin/protoc $GOPATH/bin/
+	cp -a include/google $GOPATH/src/
+	cd "$dir"
+fi
+
 
 which protoc-gen-gofast 2>/dev/null && exit 0
-dst=${TMPDIR:-/tmp}/protoc-linux_x86_64.zip
+dst="$tmpdir/protoc-linux_x86_64.zip"
 if ! [ -e "$dst" ]; then
 	curl -L -sS $(curl -sS https://github.com/google/protobuf/releases/ \
 		| sed -n -e 's/^.* href="\([^"]*linux-x86_64.zip\)".*$/https:\/\/github.com\1/p' \
@@ -16,4 +26,5 @@ if ! [ -e "$dst" ]; then
 		| sort -ur \
 		| head -n 1) -o "$dst"
 fi
-cd $HOME/bin && unzip -o "$dst"
+cd $GOPATH/bin && unzip -o "$dst"
+rm -rf "$tmpdir"
