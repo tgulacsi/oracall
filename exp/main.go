25ab8f169d0f009419950e63eb7fce21bb87beaf
Tamás Gulácsi <tgulacsi78@gmail.com>
2018-04-23 07:53:10 +0200

    ObjPrinter

--- exp/main.go
+++ exp/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"database/sql"
 	"flag"
+	"fmt"
+	"io"
 	"log"
+	"os"
 
 	"github.com/pkg/errors"
 	"gopkg.in/goracle.v2"
@@ -29,14 +32,43 @@ func Main() error {
 	}
 	defer conn.Close()
 	ot, err := conn.GetObjectType(`MMB."GDPRRequest"`)
-	log.Printf("ot=%+v error=%v", ot, err)
+	//log.Printf("ot=%+v error=%v", ot, err)
 	if err != nil {
 		return err
 	}
-	log.Printf("attrs=%+v", ot.Attributes)
-	for i, attr := range ot.Attributes {
-		log.Printf("  %d: %+v", i, attr)
-		log.Printf("sub: %+v", attr.ObjectType.Attributes)
-	}
+	(&ObjPrinter{}).Print(os.Stdout, ot)
 	return nil
+}
+
+type ObjPrinter struct {
+	Prefix  string
+	printed map[string]struct{}
+}
+
+func (p *ObjPrinter) Print(w io.Writer, ot goracle.ObjectType) {
+	if p.printed == nil {
+		p.printed = make(map[string]struct{})
+	}
+	// FIXME: print protobuf syntax
+	fmt.Fprintf(w, p.Prefix+"type %s struct {\n", ot.Info.Name())
+	later := make(map[string]goracle.ObjectType)
+	for _, attr := range ot.Attributes {
+		// FIXME: handle Collection types
+		if attr.ObjectType.Attributes == nil {
+			typ := "string"
+			switch attr.NativeTypeNum {
+			case 3004:
+			case 3005:
+				typ = "time.Time"
+			}
+			fmt.Fprintf(w, p.Prefix+"\t%s %s,\n", attr.Name, typ)
+			continue
+		}
+		fmt.Fprintf(w, p.Prefix+"\t%s %s,\n", attr.Name, attr.ObjectType.Info.Name())
+		later[attr.ObjectType.Info.Name()] = attr.ObjectType
+	}
+	fmt.Fprintf(w, p.Prefix+"}\n")
+	for _, ot := range later {
+		p.Print(w, ot)
+	}
 }
